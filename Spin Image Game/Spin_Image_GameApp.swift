cc9a383f8e00a98d112de2e2db37246a372de384
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-08-20 15:49:43 +0530

    Initial Commit

---
+++ Spin Image Game/Spin_Image_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spin_Image_GameApp.swift
+//  Spin Image Game
+//
+//  Created by Shreya Prasad on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Spin_Image_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
